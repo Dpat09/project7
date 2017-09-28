0565e1fac496cfd51f126430fb6a3db4e885086e
Dpat09 <dpat092@gmail.com>
2017-09-27 20:02:50 -0500

    Added loop for login menu, setup for switch case. Starting Login menu implementation

--- src/main/java/Login/Login.java
+++ src/main/java/Login/Login.java
@@ -1,13 +1,36 @@
 package Login;
 
+import java.util.Scanner;
+
 public class Login {
     public static void launchLogin(){
 
+        int option = 0;
+        while(true) {
+            System.out.println("\t\t========================\n" +
+                               "\t\t   WELCOME TO ACORNS!  \n" +
+                               "\t\t========================\n\n");
+            //System.out.println("========================");
+            System.out.print("Please select an option by entering 1 or 2: ");
+            try{
+                Scanner input = new Scanner(System.in);
+                option = input.nextInt();
+                if (option == 1 || option == 2){
+                    break;
+                }
 
-        System.out.println("========================\n"+
-                           "   WELCOME TO ACORNS!  \n"+
-                           "========================\n\n");
-        //System.out.println("========================");
-        System.out.println("Please select which ");
+            }catch(Exception e){
+                //Runtime.getRuntime().exec("clear");
+                System.out.println("Please enter a valid option");
+            }
+            //switch(option){
+            //    case 1: //Launch Login
+            //    case 2: //Launch SignUp
+            //}
+
+
+        }
+
+
     }
 }
