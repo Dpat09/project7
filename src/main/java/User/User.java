2fc4001ee070aab6cd67d5b60bcf82a08be65fbb
Ryan Gonzalez <ryan.gonzalez064@gmail.com>
2017-09-28 14:34:15 -0500

    Added getter/setter functions to the User.java file.

--- src/main/java/User/User.java
+++ src/main/java/User/User.java
@@ -1,6 +1,32 @@
 package User;
 
 public class User {
-    String name;
-    String password;
+   private String name;
+   private String password;
+   private String email;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail() {
+        this.email = email;
+    }
 }
+
