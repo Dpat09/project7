be3cac81cb1e814b20ee7144d1e8587747c0ea23
Dpat09 <Dpat09>
2017-11-09 17:53:27 -0600

    Created bank class to manage bank balance (non-investment balance)

---
+++ src/main/java/manageTransactions/Bank.java
@@ -0,0 +1,46 @@
+package manageTransactions;
+
+public class Bank implements moneyMovement{
+    private double balance;
+    private boolean overDraw;
+
+    public Bank(){
+        setBalance(0);
+        overDraw = false;
+    }
+
+    public Bank(double amount, boolean flag){
+        setBalance(amount);
+        setOverDraw(flag);
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+
+    private void setBalance(double amount){
+        this.balance = amount;
+    }
+
+    public boolean isOverDraw() {
+        return this.overDraw;
+    }
+
+    public void setOverDraw(boolean flag){
+        setOverDraw(flag);
+    }
+
+    public void resetOverDraw(){
+        setOverDraw(false);
+    }
+
+    public void deposit(double amount) {
+        setBalance(getBalance()+amount);
+    }
+
+    public void withDraw(double amount) {
+        double newBalance = getBalance()-amount;
+        setOverDraw(!(newBalance > 0));
+        setBalance(!isOverDraw()? getBalance()-amount:getBalance());
+    }
+}
