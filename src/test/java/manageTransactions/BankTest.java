f4b1c4963797b3c4f0cc0ad64d89b6da2fb83ffc
Dpat09 <Dpat09>
2017-11-09 23:14:05 -0600

    Started debugging tests for Bank class, created tests for all functions involved to get 100% testing. Bank class should work as intended.

--- src/test/java/manageTransactions/BankTest.java
+++ src/test/java/manageTransactions/BankTest.java
@@ -6,30 +6,46 @@ import static org.junit.Assert.assertEquals;
 
 public class BankTest {
 
+    private Bank testBank = new Bank(100, false);
+
+    @Test
+    public void canaryTest() throws Exception{
+        assert(true);
+    }
+
     @Test
     public void getBalance() throws Exception {
-        Bank testBank = new Bank(100, false);
         assertEquals(100, testBank.getBalance(), 0);
     }
 
-//    @Test
-//    public void isOverDraw() throws Exception {
-//    }
-//
-//    @Test
-//    public void setOverDraw() throws Exception {
-//    }
-//
-//    @Test
-//    public void resetOverDraw() throws Exception {
-//    }
-//
-//    @Test
-//    public void deposit() throws Exception {
-//    }
-//
-//    @Test
-//    public void withDraw() throws Exception {
-//    }
+    @Test
+    public void isOverDraw() throws Exception {
+        assertEquals(false, testBank.isOverDraw());
+    }
+
+    @Test
+    public void setOverDraw() throws Exception {
+        testBank.setOverDraw(true);
+        assertEquals(true,testBank.isOverDraw());
+    }
+
+    @Test
+    public void resetOverDraw() throws Exception {
+        testBank.setOverDraw(true);
+        testBank.resetOverDraw();
+        assertEquals(false, testBank.isOverDraw());
+    }
+
+    @Test
+    public void deposit() throws Exception {
+        testBank.deposit(15);
+        assertEquals(115,testBank.getBalance(),0);
+    }
+
+    @Test
+    public void withDraw() throws Exception {
+        testBank.withDraw(15);
+        assertEquals(85,testBank.getBalance(),0);
+    }
 
 }
