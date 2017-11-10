1872c5c150196e4a9e37fe910d0b0bf530139343
Dpat09 <Dpat09>
2017-11-10 01:11:52 -0600

    Added scanner functions for integer and doubles.

--- src/main/java/Utilities/scannerInputs.java
+++ src/main/java/Utilities/scannerInputs.java
@@ -4,14 +4,33 @@ import java.util.Scanner;
 
 public class scannerInputs {
 
-    public static void scanInput(String store){
+    public static void scanStringInput(String store){
         try{
             Scanner scanner = new Scanner(System.in);
             store = scanner.next();
             scanner.close();
         }catch (Exception e){
-            System.out.println("scanInput Error! \nPlease check Utilities >> scannerInputs >> scanInput");
+            System.out.println("scanStringInput Error! \nPlease check Utilities >> scannerInputs >> scanStringInput");
         }
+    }
 
+    public static void scanIntegerInput(int store){
+        try{
+            Scanner intScanner = new Scanner(System.in);
+            store = intScanner.nextInt();
+            intScanner.close();
+        }catch (Exception e){
+            System.out.println("scanIntegerInput Error! \nPlease check Utilities >> scannerInputs >> scanIntegerInput");
+        }
+    }
+
+    public static void scanDoubleInput(double store){
+        try{
+            Scanner doubleScanner = new Scanner(System.in);
+            store = doubleScanner.nextDouble();
+            doubleScanner.close();
+        }catch (Exception e){
+            System.out.println("scanDoubleInput Error! \nPlease check Utilities >> scannerInputs >> scanDoubleInput");
+        }
     }
 }
