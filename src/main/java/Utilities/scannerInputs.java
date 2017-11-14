88927a6545d57fe57f11ab01b34e96bd52ed471a
Dpat09 <Dpat09>
2017-11-14 12:57:06 -0600

    Added a function that utilizes Comparable to handle different data types.

--- src/main/java/Utilities/scannerInputs.java
+++ src/main/java/Utilities/scannerInputs.java
@@ -4,6 +4,16 @@ import java.util.Scanner;
 
 public class scannerInputs {
 
+    public static void scanInput(Comparable store){
+        try{
+            Scanner scanner = new Scanner(System.in);
+            store = scanner.next();
+            scanner.close();
+        }catch (Exception e){
+            System.out.println("scanInput Error! \nPlease check Utilities >> scannerInputs >> scanInput");
+        }
+    }
+
     public static void scanStringInput(String store){
         try{
             Scanner scanner = new Scanner(System.in);
