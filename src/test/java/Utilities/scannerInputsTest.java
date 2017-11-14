89ea02e7bf709f7a621975d6df77b12a0a4acaa9
Dpat09 <Dpat09>
2017-11-14 12:57:57 -0600

    Started creating Tests for scannerInputs

---
+++ src/test/java/Utilities/scannerInputsTest.java
@@ -0,0 +1,23 @@
+package Utilities;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class scannerInputsTest {
+    @Test
+    public void scanStringInput() throws Exception {
+        String store="";
+        scannerInputs.scanStringInput(store);
+        assertEquals("Working!",store);
+    }
+
+    @Test
+    public void scanIntegerInput() throws Exception {
+    }
+
+    @Test
+    public void scanDoubleInput() throws Exception {
+    }
+
+}
