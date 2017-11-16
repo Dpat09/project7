a447631cc9de9c6744b725fbf40a3a002d65cb18
Dpat09 <Dpat09>
2017-11-16 15:15:58 -0600

    Query class now contains a function to help ask user a question and returns answer as a string.

--- src/main/java/Utilities/query.java
+++ src/main/java/Utilities/query.java
@@ -3,56 +3,19 @@ package Utilities;
 import java.util.ArrayList;
 
 public class query {
-    public static <T extends Comparable<T>> Comparable queryTest(String question, ArrayList<Comparable> array, String type){
-        String store = "";
-        scannerInputs.scanInput(store);
-        //Comparable tempStore = queryHelper(type);
-        System.out.print(question);
-
-        for (int i = 0; i < array.size() ; i++){
-            if (array.get(i).compareTo(store) == 0)
-                return true;
+    public static String queryTest(String question, ArrayList<Integer> array, String store, String type) {
+        boolean flag = true;
+        while (flag) {
+            System.out.print(question);
+            store = scannerInputs.scanStringInput();
+            for (int i = 0; i < array.size(); i++) {
+                if (array.get(i).equals(Integer.parseInt(store))) {
+                    flag = false;
+                    break;
+                } else
+                    System.out.print("\nPlease choose one of the options!\n");
+            }
         }
-//        if (type.equalsIgnoreCase("int")) {
-//            scannerInputs.scanStringInput(store);
-//            tempStore = Integer.parseInt(store);
-//        }
-//        else if (type.equalsIgnoreCase("string")){
-//            scannerInputs.scanStringInput(store);
-//            tempStore = store;
-//        }
-//        else if (type.equalsIgnoreCase("double")){
-//            scannerInputs.scanStringInput(store);
-//            tempStore = Double.parseDouble(store);
-//        }
-//        else if (type.equalsIgnoreCase("char")){
-//            scannerInputs.scanStringInput(store);
-//            tempStore = Character.toUpperCase(store.charAt(0));
-//        }
-        return true;
-    }
-
-    private static Comparable queryHelper(String type){
-        String store = "";
-        Comparable tempStore = null;
-
-        if (type.equalsIgnoreCase("int")) {
-            scannerInputs.scanStringInput(store);
-            return Integer.parseInt(store);
-        }
-        else if (type.equalsIgnoreCase("string")){
-            scannerInputs.scanStringInput(store);
-            return store;
-        }
-        else if (type.equalsIgnoreCase("double")){
-            scannerInputs.scanStringInput(store);
-            return Double.parseDouble(store);
-        }
-        else if (type.equalsIgnoreCase("char")){
-            scannerInputs.scanStringInput(store);
-            return Character.toUpperCase(store.charAt(0));
-        }
-
-        return true;
+        return store;
     }
 }
