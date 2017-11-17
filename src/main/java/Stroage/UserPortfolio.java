3de8bd345ccd041738c5a803767a423d67cf3944
CallmeKingsley <kingsleyakpan18@gmail.com>
2017-11-17 17:16:48 -0600

    manipulate the user portfolio

---
+++ src/main/java/Stroage/UserPortfolio.java
@@ -0,0 +1,35 @@
+package Stroage;
+
+/**
+ * Created by Kingsley on 11/17/17.
+ */
+
+import User.User;
+
+public class UserPortfolio {
+
+    protected String aggression  = "";
+    protected String investment_interval = "";
+    protected  double investment_amount = 0.00;
+    protected  boolean interval_switch = false;
+    private double funds = 0.00;
+
+
+
+
+    public UserPortfolio(User currentUser){
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
