d7b5e27a7212b91e21c11f3a20933d83a93c1680
Dpat09 <Dpat09>
2017-11-18 13:54:11 -0600

    Temporarily commented out code.

--- src/main/java/manageTransactions/investInterval.java
+++ src/main/java/manageTransactions/investInterval.java
@@ -3,28 +3,19 @@ package manageTransactions;
 import Utilities.scannerInputs;
 
 public class investInterval extends Portfolio{
+
     public void setreoccuring(Portfolio obj){
 
-        obj.investment_interval = scannerInputs.scanStringInput();
-
-//        try{
-//            Scanner input = new Scanner(System.in);
-//            obj.investment_interval = input.nextLine();
-//            input.close();
-//        }
-//        catch(Exception e){
-//            System.out.println("Error setting investment interval");
-//        }
+        //obj.investmentInterval = scannerInputs.scanStringInput();
 
     }
 
     public boolean investAmount(Portfolio obj, double newAmount){
-        obj.investment_amount = newAmount>=1 ? newAmount : obj.investment_amount;
+        //obj.investmentAmount = newAmount>=1 ? newAmount : obj.investmentAmount;
         return newAmount>=1;
     }
     public void setSwitch(Portfolio obj, boolean flag){
-        obj.interval_switch = flag;
+        //obj.intervalSwitch = flag;
     }
-
 
 }
