ab5bf7c83187fe3d988b7c45599e5af5b46c56f5
Dpat09 <Dpat09>
2017-11-18 13:01:18 -0600

    Fixed broken functions.

--- src/main/java/manageTransactions/Portfolio.java
+++ src/main/java/manageTransactions/Portfolio.java
@@ -1,42 +1,43 @@
 package manageTransactions;
 
-import Utilities.query;
+import Utilities.queryHandler;
 import Utilities.scannerInputs;
 
-import java.util.ArrayList;
-
 public class Portfolio implements moneyMovement{
-    protected String aggression = ""; //portfolio type (conservative to aggressive)
-    protected String investment_interval = "";
-    protected double investment_amount = 0.00;
-    protected boolean interval_switch = false;
+    protected int aggression; // 1 for Conservative, 2 for Moderate, 3 for Aggressive
+    protected int investmentInterval;
+    protected double investmentAmount;
+    protected boolean intervalSwitch;
     private double funds = 0.00;
 
-    private ArrayList<Integer> aggressionList = new ArrayList<Integer>();
     public Portfolio() {
-        aggressionList.add(1);    //Conservative
-        aggressionList.add(2);    //Moderate
-        aggressionList.add(3);    //Aggressive
+
+    }
+
+    public Portfolio(int aggression, int investmentInterval, double investmentAmount, boolean intervalSwitch, double funds){
+        this.aggression = aggression;
+        this.investmentInterval = investmentInterval;
+        this.investmentAmount = investmentAmount;
+        this.intervalSwitch = intervalSwitch;
+        setFunds(funds);
     }
 
     public double getFunds() {
         return this.funds;
     }
-    public boolean getIntervalSwitch(){return this.interval_switch;}
-    public double getInvestmentAmount(){return this.investment_amount;}
-    public String getInvestmentInterval(){return this.investment_interval;}
-    public String getAggression(){return this.aggression;}
+    public boolean getIntervalSwitch(){return this.intervalSwitch;}
+    public double getInvestmentAmount(){return this.investmentAmount;}
+    public int getInvestmentInterval(){return this.investmentInterval;}
+    public int getAggression(){return this.aggression;}
+
     public void setFunds(double funds) {
         this.funds = funds;
     }
 
-    public void setAggression(){
-        String question = "Set aggression on portfolio: \n\n1.) Conservative \n2.) Moderate \n3.) Aggressive \n\nEnter Choice:";
-        //int input = query.optionsQuery(question,aggressionList);
-        //System.out.println(scannerInputs.scanStringInput()+scannerInputs.scanStringInput());
-        String name = scannerInputs.scanStringInput();
-        String name1 = scannerInputs.scanStringInput();
-        System.out.println(name+name1);
+    public void changeAggression(){
+        String question = "Set aggression on portfolio: \n\n1.) Conservative \n2.) Moderate \n3.) Aggressive \n4.) Cancel \n\nEnter Choice:";
+        int input = queryHandler.optionsQuery(question,4);
+        aggression = input != 4 ? input : aggression;
     }
 
     public void deposit(double amount) {
@@ -44,6 +45,9 @@ public class Portfolio implements moneyMovement{
     }
 
     public void withDraw(double amount) {
-        setFunds((getFunds()-amount)>0?getFunds()-amount:getFunds());
+        if ((getFunds() - amount) > 0)
+            setFunds((getFunds() - amount));
+        else
+            System.out.println("Insufficient funds!\n"+"You cannot withdraw "+amount);
     }
 }
