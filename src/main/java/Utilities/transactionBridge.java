d319c9b526bdd7375eee918a9e51340a0358ae9e
Dpat09 <Dpat09>
2017-11-20 21:21:26 -0600

    Fixed possibility of depositing money not available in bank, and the same for pulling money from investment account.

--- src/main/java/Utilities/transactionBridge.java
+++ src/main/java/Utilities/transactionBridge.java
@@ -5,11 +5,11 @@ import manageTransactions.Portfolio;
 
 public class transactionBridge {
     public static void bankToPortfolio(Bank bankAccount, Portfolio currentPortfolio, double amount){
-        bankAccount.withDraw(amount);
-        currentPortfolio.deposit(amount);
+        if (bankAccount.withDraw(amount))
+            currentPortfolio.deposit(amount);
     }
     public static void portfolioToBank(Bank bankAccount, Portfolio currentPortfolio, double amount){
-        currentPortfolio.withDraw(amount);
-        bankAccount.deposit(amount);
+        if (currentPortfolio.withDraw(amount))
+            bankAccount.deposit(amount);
     }
 }
