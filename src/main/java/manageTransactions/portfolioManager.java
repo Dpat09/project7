69201a9607c80aefaea3c005c999d45167d9c9bf
Dpat09 <Dpat09>
2017-11-26 16:40:27 -0600

    Started setup menu for Portfolio manager.

--- src/main/java/manageTransactions/portfolioManager.java
+++ src/main/java/manageTransactions/portfolioManager.java
@@ -6,6 +6,26 @@ import Utilities.transactionBridge;
 
 public class portfolioManager extends transactionBridge {
 
+    public static void portfolioManagerMenu(){
+        int input = 0;
+        String title = "\n\n\n\t\t========================\n" +
+                "\t        Manage Portfolio        \n" +
+                "\t\t========================\n\n";
+        String question = "1.) See investment breakdown"
+                + "\n2.) Predicted investment trajectory"
+                + "\n3.) Change Aggression"
+                + "\n4.) Go back to Main Menu"
+                + "\nEnter Choice: ";
+        while (input != 4) {
+            input = queryHandler.optionsQuery(title + question, 4);
+            switch(input){
+                case 1:
+                case 2:
+                case 3:
+            }
+        }
+    }
+
     public static Portfolio createPortfolio(Portfolio portObj, Bank bankObj){
         System.out.println("\n\n\n\t\t========================\n" +
                 "\t       Create a Portfolio!        \n" +
@@ -25,7 +45,7 @@ public class portfolioManager extends transactionBridge {
 
     public static void addFunds(Portfolio portObj, Bank bankObj){
         System.out.print("\nAdd money to Investment Funds\n\nEnter amount to add from bank:");
-        double amount = scannerInputs.scanDoubleInput();
+        double amount = scannerInputs.scanDouble();
         transactionBridge.bankToPortfolio(bankObj,portObj,amount);
     }
 
