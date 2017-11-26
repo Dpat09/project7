bd380c9d0d94d09e9230604f2c62fb0e3c23645d
Dpat09 <Dpat09>
2017-11-26 16:35:03 -0600

    New class to manage user object. Provides menus to be called from dashboard.

---
+++ src/main/java/User/userManager.java
@@ -0,0 +1,42 @@
+package User;
+
+import static Utilities.queryHandler.*;
+
+public class userManager {
+    public static void userManagerMenu(User currentUser){
+        int input = 0;
+        String title = "\n\n\n\t\t========================\n" +
+                "\t        Manage User             \n" +
+                "\t\t========================\n\n";
+        String question = "1.) Change password"
+                +"\n2.) Go back to Main Menu"
+                +"\nEnter Choice: ";
+
+        while (input !=2){
+            input = optionsQuery(title+question,2);
+            switch (input){
+                case 1:changeUserPass(currentUser);
+            }
+        }
+    }
+
+    private static void changeUserPass(User currentUser){
+        while (true){
+            System.out.println("\n\n\n\t\t========================\n" +
+                    "\t        Change Password          \n" +
+                    "\t\t========================\n\n");
+
+            String oldPass = inputInfo("Enter current password:");
+            String newPass = inputInfo("Enter new password:");
+
+            if (oldPass.equals(currentUser.getPassword())){
+                currentUser.setPassword(newPass);
+                break;
+            }
+            else
+                System.out.println("\n\nThe current password did not match.\nPlease try again.");
+        }
+
+    }
+
+}
