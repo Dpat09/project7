98aa37b942221226d21fcfc92f4d0906bfcca32d
ryan.gonzalez64@gmail.com <ryan.gonzalez64@gmail.com>
2017-11-27 00:50:32 -0600

    Next update for stocks. Working on calculating value of investment based on aggression type.

--- src/main/java/Stock/stockCalc.java
+++ src/main/java/Stock/stockCalc.java
@@ -4,18 +4,40 @@ import User.User;
 import manageTransactions.Bank;
 import manageTransactions.Portfolio;
 import Utilities.readingWrite;
+import yahoofinance.*;
+import java.io.IOException;
+import java.math.BigDecimal;
 
 public class stockCalc {
-    public void investmentCalc(Portfolio portfolio){
+    public void investmentCalc(Portfolio portfolio) throws IOException {
         int aggression = portfolio.getAggression();
         if (aggression == 1){
-            //aggression is conservative, calculate investment based on conservative stocks.
+            Stock Busch = YahooFinance.get("BUD");
+            Stock BankOfAmerica = YahooFinance.get("BAC");
+            Stock JohnsonJohnson = YahooFinance.get("JNJ");
+
+            BigDecimal poBusch = Busch.getQuote(true).getPrice();
+            BigDecimal poBankOfAmerica = BankOfAmerica.getQuote(true).getPrice();
+            BigDecimal poJohnsonJohnson = JohnsonJohnson.getQuote(true).getPrice();
+
         }
         else if (aggression == 2){
-            //aggression is moderate, calculate investment based on moderate stocks.
+            Stock CVS = YahooFinance.get("CVS");
+            Stock Walgreens = YahooFinance.get("WBA");
+            Stock Walmart = YahooFinance.get("WMT");
+
+            BigDecimal poCVS = CVS.getQuote(true).getPrice();
+            BigDecimal poWalgreens = Walgreens.getQuote(true).getPrice();
+            BigDecimal poWalmart = Walmart.getQuote(true).getPrice();
         }
         else if (aggression == 3){
-            //aggression is aggressive, calculate investment based on aggressive stocks.
+            Stock TEVA = YahooFinance.get("TEVA");
+            Stock ScientificGames = YahooFinance.get("SGMS");
+            Stock SuperValueGrocery = YahooFinance.get("SVU");
+
+            BigDecimal poTEVA = TEVA.getQuote(true).getPrice();
+            BigDecimal poScientificGames = ScientificGames.getQuote(true).getPrice();
+            BigDecimal poSuperValueGames = SuperValueGrocery.getQuote(true).getPrice();
         }
     }
 }
