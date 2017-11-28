70c51e86a8a245d19c4b7ddd1e9dab632b6d6194
Dpat09 <Dpat09>
2017-11-28 13:15:25 -0600

    ScannerInputs are now tested to be working.

--- src/test/java/Utilities/scannerInputsTest.java
+++ src/test/java/Utilities/scannerInputsTest.java
@@ -2,22 +2,35 @@ package Utilities;
 
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 import static org.junit.Assert.*;
 
 public class scannerInputsTest {
-//    @Test
-//    public void scanStringInput() throws Exception {
-//        String store="";
-//        scannerInputs.scanStringInput(store);
-//        assertEquals("Working!",store);
-//    }
 
     @Test
-    public void scanIntegerInput() throws Exception {
+    public void scanString() throws Exception {
+        String input = "hello";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        assertEquals("hello",scannerInputs.scanString());
     }
 
     @Test
-    public void scanDoubleInput() throws Exception {
+    public void scanInteger() throws Exception {
+        String input = "10";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        assertEquals(10,scannerInputs.scanInteger());
+    }
+
+    @Test
+    public void scanDouble() throws Exception {
+        String input = "10.55";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        assertEquals(10.55,scannerInputs.scanDouble(),0);
     }
 
 }
