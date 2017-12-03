e43f944062d716cd8ba2acf5a6946ebdee2482a1
Dpat09 <dpat092@gmail.com>
2017-12-03 01:31:55 -0600

    Added more tests to UserTest. 50% done

--- src/test/java/User/UserTest.java
+++ src/test/java/User/UserTest.java
@@ -5,16 +5,35 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class UserTest {
+
+    private User userObj = new User("test","123","123@g.com",false);
+
+    @Test
+    public void canary() throws Exception{
+        assert(true);
+    }
+
+    @Test
+    public void creatObj() throws Exception{
+        User test = new User();
+        assert(true);
+    }
+
     @Test
     public void getName() throws Exception {
+        assertEquals("test",userObj.getName());
     }
 
     @Test
     public void setName() throws Exception {
+        userObj.setName("testing");
+        assert(true);
+        assertEquals("testing",userObj.getName());
     }
 
     @Test
     public void getPassword() throws Exception {
+        assertEquals("123",userObj.getPassword());
     }
 
     @Test
