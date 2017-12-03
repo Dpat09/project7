39a9a78865e0bc44a86b72f35019588890b6bbdc
Dpat09 <dpat092@gmail.com>
2017-12-03 01:17:08 -0600

    Started creating tests for User.

---
+++ src/test/java/User/UserTest.java
@@ -0,0 +1,40 @@
+package User;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserTest {
+    @Test
+    public void getName() throws Exception {
+    }
+
+    @Test
+    public void setName() throws Exception {
+    }
+
+    @Test
+    public void getPassword() throws Exception {
+    }
+
+    @Test
+    public void setPassword() throws Exception {
+    }
+
+    @Test
+    public void getEmail() throws Exception {
+    }
+
+    @Test
+    public void setEmail() throws Exception {
+    }
+
+    @Test
+    public void isCorporate() throws Exception {
+    }
+
+    @Test
+    public void setCorporate() throws Exception {
+    }
+
+}
