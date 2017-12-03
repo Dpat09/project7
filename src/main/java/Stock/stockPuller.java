d1597e297ccb9dddcda770623cdf20271b53dd83
Dpat09 <dpat092@gmail.com>
2017-12-03 00:01:41 -0600

    Moved all try catch down to stockPuller.

--- src/main/java/Stock/stockPuller.java
+++ src/main/java/Stock/stockPuller.java
@@ -1,13 +1,19 @@
 package Stock;
 
+import com.sun.javafx.applet.ExperimentalExtensions;
 import yahoofinance.Stock;
 import yahoofinance.YahooFinance;
 
 import java.io.IOException;
 
 public class stockPuller {
-    public static double stockPull(String stock) throws IOException {
-        Stock generic = YahooFinance.get(stock);
-        return generic.getQuote(true).getPrice().doubleValue();
+    public static double stockPull(String stock){
+        try{
+            Stock generic = YahooFinance.get(stock);
+            return generic.getQuote(true).getPrice().doubleValue();
+        }catch (Exception e){
+        }
+        System.out.println("error in stockPuller");
+        return 0;
     }
 }
