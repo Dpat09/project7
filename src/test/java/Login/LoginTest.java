78e01b4a079bf096296d4eae97000732bb94db5a
Dpat09 <dpat092@gmail.com>
2017-12-03 22:42:18 -0600

    Added object creation, and launchLoginMenu test.

--- src/test/java/Login/LoginTest.java
+++ src/test/java/Login/LoginTest.java
@@ -3,6 +3,9 @@ package Login;
 import User.User;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 import static org.junit.Assert.*;
 
 public class LoginTest {
@@ -15,7 +18,18 @@ public class LoginTest {
     }
 
     @Test
+    public void createObj() throws Exception{
+        Login test = new Login();
+        assert(true);
+    }
+
+    @Test
     public void launchLoginMenu() throws Exception {
+        String input = "4";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        Login.launchLoginMenu(userObj);
+        assert(true);
     }
 
     @Test
