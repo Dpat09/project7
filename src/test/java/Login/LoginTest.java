d832d4e54b5c25aa23ddb4863459c5066c5657ce
Dpat09 <dpat092@gmail.com>
2017-12-03 22:39:49 -0600

    Started Login tests, added canary

---
+++ src/test/java/Login/LoginTest.java
@@ -0,0 +1,29 @@
+package Login;
+
+import User.User;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LoginTest {
+
+    private User userObj = new User("test1","123","123test@g.com",false);
+
+    @Test
+    public void canary() throws Exception{
+        assert(true);
+    }
+
+    @Test
+    public void launchLoginMenu() throws Exception {
+    }
+
+    @Test
+    public void emailValidation() throws Exception {
+    }
+
+    @Test
+    public void varifidationHelper() throws Exception {
+    }
+
+}
