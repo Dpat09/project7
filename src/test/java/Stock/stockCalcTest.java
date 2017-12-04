60603576d985743198b069f8bf295339b953b99b
Dpat09 <dpat092@gmail.com>
2017-12-03 21:16:06 -0600

    Added many tests, and made setup function more useful for more tests.

--- src/test/java/Stock/stockCalcTest.java
+++ src/test/java/Stock/stockCalcTest.java
@@ -10,22 +10,47 @@ public class stockCalcTest extends stockCalc {
     private Portfolio portObj = new Portfolio(1,100,.5,false,110);
     private stockStore stockObj = new stockStore();
 
-    private void setupTest(int aggression){
+    private void setupTest(int aggression, boolean calc){
         portObj.setAggression(aggression);
         stockCalc.setStocks(portObj,stockObj);
-        stockCalc.investmentCalc(portObj,stockObj);
+        if (calc)
+            stockCalc.investmentCalc(portObj,stockObj);
+    }
+
+    @Test
+    public void creatObj(){
+        stockCalc test = new stockCalc();
     }
 
     @Test
     public void investmentCalc1() throws Exception {
+        setupTest(1,true);
+        assert(true);
+    }
+
+    @Test
+    public void investmentCalc2() throws Exception {
+        setupTest(2,true);
+        assert(true);
+    }
+
+    @Test
+    public void investmentCalc3() throws Exception {
+        setupTest(3,true);
+        assert(true);
     }
 
     @Test
     public void changeInPercent() throws Exception {
+        stockObj.setInitPrice("BUD",10);
+        double newStock = 10;
+        assertEquals(0,changeInPercent(stockObj,newStock,"BUD"),0);
     }
 
     @Test
     public void setStocks1() throws Exception {
+        setupTest(1,false);
+        assert(true);
     }
 
     @Test
