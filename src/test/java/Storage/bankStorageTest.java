662acb0e95e6f62222a7d6c931bf3d129a70368c
Dpat09 <dpat092@gmail.com>
2017-12-03 21:56:33 -0600

    removed useless objects

--- src/test/java/Storage/bankStorageTest.java
+++ src/test/java/Storage/bankStorageTest.java
@@ -11,9 +11,7 @@ import static org.junit.Assert.*;
 public class bankStorageTest {
 
     private User userObj = new User("test1","123","123test@g.com",false);
-    private Portfolio portObj = new Portfolio(1,100,.5,false,110);
     private Bank bankObj = new Bank(100,false);
-    private stockStore stockObj = new stockStore();
 
     @Test
     public void canary() throws Exception{
