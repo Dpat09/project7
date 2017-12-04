4c3ba37c66e6b4209feed99a9bb1e6d94d9440f8
Dpat09 <dpat092@gmail.com>
2017-12-03 21:54:26 -0600

    Created tests for bankStorage, 100% coverage

---
+++ src/test/java/Storage/bankStorageTest.java
@@ -0,0 +1,40 @@
+package Storage;
+
+import Stock.stockStore;
+import User.User;
+import manageTransactions.Bank;
+import manageTransactions.Portfolio;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class bankStorageTest {
+
+    private User userObj = new User("test1","123","123test@g.com",false);
+    private Portfolio portObj = new Portfolio(1,100,.5,false,110);
+    private Bank bankObj = new Bank(100,false);
+    private stockStore stockObj = new stockStore();
+
+    @Test
+    public void canary() throws Exception{
+        assert(true);
+    }
+
+    @Test
+    public void createObj() throws Exception{
+        bankStorage test = new bankStorage();
+        assert(true);
+    }
+
+    @Test
+    public void writeFile() throws Exception {
+        bankStorage.writeFile(bankObj,userObj);
+        assert(true);
+    }
+
+    @Test
+    public void readFile() throws Exception {
+        bankStorage.readFile(bankObj,userObj);
+        assertEquals(100,bankObj.getBalance(),0);
+    }
+}
