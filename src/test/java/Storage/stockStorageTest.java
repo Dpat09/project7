e95cbf828290b2406cd5625a17a6e01ba0c61ea3
Dpat09 <dpat092@gmail.com>
2017-12-03 22:02:24 -0600

    Started creating Test

---
+++ src/test/java/Storage/stockStorageTest.java
@@ -0,0 +1,16 @@
+package Storage;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class stockStorageTest {
+    @Test
+    public void writeFile() throws Exception {
+    }
+
+    @Test
+    public void readFile() throws Exception {
+    }
+
+}
